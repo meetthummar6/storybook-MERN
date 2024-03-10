febe3526de7393fe100bb45c07a5fb8ce215df51
Meet Thummar <meetthummar132@gmail.com>
2024-03-10 23:49:14 +0530

    Basic setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl text-blue-700'>
+        Hello world
+      </div>
+      
+    </>
+  )
+}
+
+export default App
